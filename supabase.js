22f6c48e39c400b72c17ee7db429034c4bae3d55
insurancetemp <adamnottz304@gmail.com>
2026-07-29 06:40:08 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://lvjdaizpaqjnchjvrsni.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_fnag8tkEEClWTLVR7fL1sQ_3_rbAzEc";
 
-const supabaseClient = supabase.createClient(
+const supabase= window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_ANON_KEY
 );
